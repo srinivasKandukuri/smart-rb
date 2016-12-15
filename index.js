7aec10706c6778fd3d9ffde7af6f14be7a04cd5b
SK <ksrinivas.cse@gmail.com>
2016-12-15 21:11:33 +0530

    solr call to get listings

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const bodyParser = require('body-parser');
 const restService = express();
 restService.use(bodyParser.json());
 var path = require('path');
+
+var http = require("http");
+
 restService.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 
 
 restService.post('/hook', function (req, res) {
@@ -27,7 +31,34 @@ restService.post('/hook', function (req, res) {
             }
 
             if(requestBody.result.action == 'listings'){
-                //speech = listings;
+                var query = 'https://search-build.equipmentone.com/e1_search?wt=json&q=*&fq={!tag=mar}marketplace:(%22EquipmentOne%22)&fq=auctionEndDateTimeUTC:([NOW%20TO%20*])&sort=siteSort%20asc,auctionEndDateTimeUTC%20asc,reserveType%20asc,highBidAmount%20desc&rows=5';
+                // get is a simple wrapper for request()
+                // which sets the http method to GET
+                var request = http.get(query, function (response) {
+                    // data is streamed in chunks from the server
+                    // so we have to handle the "data" event    
+                    var buffer = "", 
+                        data,
+                        route;
+
+                    response.on("data", function (chunk) {
+                        buffer += chunk;
+                    }); 
+
+                    response.on("end", function (err) {
+                        // finished transferring data
+                        // dump the raw data
+                        console.log(buffer);
+                        console.log("\n");
+                        data = JSON.parse(buffer);
+                        route = data.routes[0];
+
+                        // extract the distance and time
+                        console.log("Walking Distance: " + route.legs[0].distance.text);
+                        console.log("Time: " + route.legs[0].duration.text);
+                        console.log(data);
+                    }); 
+                }); 
             }
 
 
